99cde2a17d8010c8ec1ee87a9dde974b7fd94097
kimsanghyup <shkim@fashiongo.net>
2015-04-15 17:17:50 +0900

    my first git project test01

---
+++ WebApplication1/Controllers/AuthenticateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Web.Security;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthenticateController : ApiController
+    {
+        // GET api/values/5
+        //[Route("fg2lambs/auth")]
+        [HttpGet]
+        public bool Get(string uid, string pwd)
+        {
+            bool b = Membership.ValidateUser(uid, pwd);
+            return b;
+        }
+    }
+}
